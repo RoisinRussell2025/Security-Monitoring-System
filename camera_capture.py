eb303a17be14316eb077b53e4ca595349b3a7acf
RoisinRussell2025 <roisin_russell@yahoo.ie>
2026-05-18 13:31:16 +0100

    Phyton code update

--- camera_capture.py
+++ camera_capture.py
@@ -1,2 +1,34 @@
+
+
 print("Project started")
 print("Camera tested successfully")
+
+from picamera2 import Picamera2
+import time
+import json
+from datetime import datetime
+
+# Start camera
+picam2 = Picamera2()
+picam2.start()
+
+while True:
+    # Capture image
+    image_path = "static/last_capture.jpg"
+    picam2.capture_file(image_path)
+
+    # Create event data
+    data = {
+        "timestamp": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+        "event": "image_captured"
+    }
+
+    # Save JSON data
+    with open("data/data.json", "w") as f:
+        json.dump(data, f)
+
+    # Print output (important for assignment)
+    print(f"Captured image at {data['timestamp']}")
+
+    # Wait 10 seconds
+    time.sleep(10)
